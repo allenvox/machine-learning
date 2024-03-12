34647a4d4cf583c8886c70c4d10432ac49aeb177
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-03-12 08:31:10 +0700

    Comment code, change sample dataset generation

--- src/logistic-regression.py
+++ src/logistic-regression.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 class LogisticRegression:
     def __init__(self, learning_rate=0.01, num_iterations=100000, fit_intercept=True, verbose=False):
+        # initializing logistic regression model parameters
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
         self.fit_intercept = fit_intercept
@@ -16,6 +17,7 @@ class LogisticRegression:
         return 1 / (1 + np.exp(-z))
 
     def __loss(self, h, y):
+        # logistic regression loss function (L)
         return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
 
     def fit(self, X, y):
@@ -23,6 +25,7 @@ class LogisticRegression:
             X = self.__add_intercept(X)
         # weights initialization
         self.theta = np.zeros(X.shape[1])
+        # gradient descent for fitting the weights
         for i in range(self.num_iterations):
             z = np.dot(X, self.theta)
             h = self.__sigmoid(z)
@@ -36,15 +39,16 @@ class LogisticRegression:
     def predict_prob(self, X):
         if self.fit_intercept:
             X = self.__add_intercept(X)
+        # return sigmoid function for X values and learned theta
         return self.__sigmoid(np.dot(X, self.theta))
 
     def predict(self, X, threshold=0.5):
         return self.predict_prob(X) >= threshold
 
-# Generate more sample data with higher standard deviation
-np.random.seed(0)
-X1 = np.random.randn(100, 2) * 2 + [2, 2]
-X2 = np.random.randn(100, 2) * 2 + [-2, -2]
+# Generate sample data with standard deviation
+np.random.seed(42)
+X1 = np.random.randn(100, 2) * 1.4 + [4, 4]
+X2 = np.random.randn(100, 2) * 1.4 + [-4, -4]
 X = np.vstack([X1, X2])
 y = np.hstack([np.zeros(100), np.ones(100)])
 
